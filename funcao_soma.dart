0eb785669cbed132fc2fcb5bc7d05da50f087338
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-05 09:56:32 -0300

    funcao_soma.dart

--- funcao_soma.dart
+++ funcao_soma.dart
@@ -1,14 +1,18 @@
 import 'dart:io';
 
 void main() {
-  var soma = somaNumeros();
-  print(soma);
-}
-
-int somaNumeros (){
+  
   stdout.write("DIGITE O PRIMERO VALOR: ");
   int N1 = int.parse(stdin.readLineSync()!);
   stdout.write("DIGITE O PRIMERO VALOR: ");
   int N2 = int.parse(stdin.readLineSync()!);
-  return N1 + N2;
+  
+  var soma = somaNumeros(N1,N2);
+  print(soma);
+}
+
+int somaNumeros (N1,N2){
+  int soma = 0;
+  soma = N1 + N2;
+  return soma;
 }
