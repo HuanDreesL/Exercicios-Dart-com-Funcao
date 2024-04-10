05b87ca385219e399ff1d83fc08c37774b16ffe2
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-10 10:50:39 -0300

    concatenacao.dart

---
+++ concatenacao.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main(){
+  stdout.write("insira uma quantidade: ");
+  int quant = int.parse(stdin.readLineSync()!);
+
+  List<String> palavras = [];
+  for(int i = 0; i < quant; i++){
+    stdout.write("Escreva: ");
+    String escreva = stdin.readLineSync()!;
+    palavras.add(escreva);
+  }
+  var mensagem = concatenacao(palavras);
+  print(mensagem);
+}
+  String concatenacao(List<String> palavras){
+    String juntarString = '';
+    for(int i = 0; i < palavras.length; i++){
+      juntarString = juntarString + palavras[i];
+    }
+    return juntarString;
+  }
