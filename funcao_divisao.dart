d873cd49ebbb202bcaa0601307e639b36c8e987f
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-11 11:04:28 -0300

    funcao_divisao.dart

--- funcao_divisao.dart
+++ funcao_divisao.dart
@@ -5,16 +5,16 @@ void main(){
   double N1 = double.parse(stdin.readLineSync()!);
   stdout.write("Insira o Segundo Valor: ");
   double N2 = double.parse(stdin.readLineSync()!);
+  
+  var mensagem = divisaoValores(N1, N2);
+  print(mensagem);
 
-  if(N2 == 0){
-    print("Não é possivel dividir número por ZERO!");
-  }else{
-    var mensagem = divisaoValores(N1, N2);
-    print(mensagem);
-  }
 }
   double divisaoValores(double N1,N2){
     double divisao;
+    if(N2 == 0){
+      print("Não é possivel dividir número por ZERO!");
+    }
     divisao = N1 / N2; 
     return divisao;
   }
