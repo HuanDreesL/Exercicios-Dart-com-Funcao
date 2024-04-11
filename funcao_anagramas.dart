28dcf19775373df46adebd11898dee8555d3eed3
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-11 10:03:17 -0300

    funcao_anagramas.dart

--- funcao_anagramas.dart
+++ funcao_anagramas.dart
@@ -1,27 +1,37 @@
 import 'dart:io';
 void main(){
+  String palavra1,palavra2;
 
-  stdout.write("Escreva a Primeira Palavra: ");
-  String palavra1 = stdin.readLineSync()!;
-  stdout.write("Escreva a Primeira Palavra: ");
-  String palavra2 = stdin.readLineSync()!;
+  stdout.write("Escreva: ");
+  palavra1 = stdin.readLineSync()!;
+  stdout.write("Escreva: ");
+  palavra2 = stdin.readLineSync()!;
 
   var mensagem = anagramas(palavra1, palavra2);
   print(mensagem);
+
 }
-  bool anagramas(String palavra1, String palavra2){
-    bool ehAnagramas = true;
-      if(palavra1.length != palavra2.length){
-          bool ehAnagramas = false;
-      }
-      if(ehAnagramas){
-        print("A palavra é um anagrama");
-      }
-      else{
-        print("A palavra não é um anagrama");
-      }
-    return ehAnagramas;      
-  }
-    
-    
+bool anagramas(String palavra1, String palavra2){
+  bool ehanagrama = true;
   
+  if(palavra1.length != palavra2.length){
+    ehanagrama = false;
+  }
+  List<String> lista1 = palavra1.split(''); // divide em lista de caracteres
+  List<String> lista2 = palavra2.split('');
+  lista1.sort(); // depois ordena elas em ordem alfabetica 
+  lista2.sort();
+
+  for(int i = 0; i < lista1.length; i++){
+    if(lista1[i] != lista2[i]){ 
+      ehanagrama = false;
+      break;
+    }
+  }
+  if(ehanagrama){
+    print("A palavra $palavra1 e a palavra $palavra2 são anagramas?");
+  }else{
+    print("A palavra $palavra1 e a palavra $palavra2 são anagramas?");
+  }
+  return ehanagrama;
+}
