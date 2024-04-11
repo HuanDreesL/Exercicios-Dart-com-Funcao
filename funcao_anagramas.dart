13b5bd3e52b97f579db7e16e16decf832072751f
Huandres Schmidt <163208912+HuanDreesL@users.noreply.github.com>
2024-04-11 11:43:35 -0300

    funcao_anagramas.dart

--- funcao_anagramas.dart
+++ funcao_anagramas.dart
@@ -1,37 +1,46 @@
 import 'dart:io';
 void main(){
-  String palavra1,palavra2;
-
-  stdout.write("Escreva: ");
+  String palavra1;
+  String palavra2;
+  int dec;
+  do{
+  stdout.write("Escreva a 1° Palavra: ");
   palavra1 = stdin.readLineSync()!;
-  stdout.write("Escreva: ");
+  stdout.write("Escreva a 2° Palavra: ");
   palavra2 = stdin.readLineSync()!;
 
-  var mensagem = anagramas(palavra1, palavra2);
+  var mensagem = verificAranagrama(palavra1, palavra2);
   print(mensagem);
 
-}
-bool anagramas(String palavra1, String palavra2){
-  bool ehanagrama = true;
+  stdout.write("Deseja fazer uma nova tentativa?\n1-Sim\n2-Não\n:");
+  dec = int.parse(stdin.readLineSync()!);
+  }while(dec == 1);
   
+}
+bool verificAranagrama(String palavra1,String palavra2){
+  bool ehanagrama = true;
+
   if(palavra1.length != palavra2.length){
     ehanagrama = false;
-  }
-  List<String> lista1 = palavra1.split(''); // divide em lista de caracteres
-  List<String> lista2 = palavra2.split('');
-  lista1.sort(); // depois ordena elas em ordem alfabetica 
-  lista2.sort();
+  } else {
+    List<String> lista1 = palavra1.split('');
+    List<String> lista2 = palavra2.split('');
+    lista1.sort();
+    lista2.sort();
 
-  for(int i = 0; i < lista1.length; i++){
-    if(lista1[i] != lista2[i]){ 
-      ehanagrama = false;
-      break;
+    for(int i = 0; i < lista1.length; i++){
+      if(lista1[i] != lista2[i]){
+        ehanagrama = false;
+        break;
+      } 
     }
   }
   if(ehanagrama){
-    print("A palavra $palavra1 e a palavra $palavra2 são anagramas?");
-  }else{
-    print("A palavra $palavra1 e a palavra $palavra2 são anagramas?");
+    print("As palavras escritas 1° $palavra1 e 2° $palavra2 são anagramas?");
+  }
+  else{
+    print("As palavras escritas 1° $palavra1 e 2° $palavra2 são anagramas?");
   }
   return ehanagrama;
+  
 }
